1a518aae9d49d97f497ec52dec48726ffd57b7a2
Nikolajczyk Marcin <marcin.nikolajczyk@tieto.com>
2018-08-22 09:39:45 +0200

    Client can sort films by passing key as a argument. Changed format of date

--- API.py
+++ API.py
@@ -1,10 +1,32 @@
 import omdb
 from omdb import OMDBClient
 import re
-import json
+from datetime import datetime
+import time
 
 omdb.set_default('apikey',"81c13a05")
 running = 1
+i = 0
+list_of_requests = []
+
+#def functionToSort(list):
+#     for i in list:
+#        if re.fullmatch(type_of_sorting, i):
+#            print(i)
+#            return i
+
+
+def sort_key(d):
+    if type_of_sorting == "Rating":
+        return d['imdb_rating']
+    elif type_of_sorting == "Length":
+        return d['runtime']
+    elif type_of_sorting == "Release_date":
+        return d['released']
+    elif type_of_sorting == "Popularity":
+        return d['imdb_votes']
+
+
 while running:
     print("Podaj tytul/tytuly filmu")
     title = input()
@@ -18,9 +40,15 @@ while running:
         type_of_sorting = type_of_sorting[1:]
     title_as_list = list(title.split(', '))
     for x in title_as_list:
-        print(x)
         res = omdb.get(title=x)
-        for key, value in res.items():
+        list_of_requests.append(res)
+    list_of_requests.sort(key=sort_key, reverse=True)
+    for y in list_of_requests:
+        for key, value in list_of_requests[i].items():
+            if key == "released" and value != "N/A":
+                value = datetime.strptime(value, '%d %b %Y').strftime("%d.%m.%Y")
             print(f"{key}: {value}")
+        i += 1
         print("\n")
-
+    i = 0
+    list_of_requests = []
